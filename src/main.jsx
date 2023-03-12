9ad568425c8be84acb9c241e4c38e9f7728f7942
ZennithYattare <luis.espera.0758@gmail.com>
2023-03-12 08:42:58 +0800

    omit createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+//  https://forum.freecodecamp.org/t/react-18-destroys-25-5-clock-tests-front-end-development-libraries-projects-build-a-25-5-clock/528192
+//  Known issue with React 18 and ReactDOM.createRoot with this specific project, need to use the following instead:
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
